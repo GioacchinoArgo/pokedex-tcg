33ad315cdbd28b390644b1e274e5b47caf82fdd3
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-11-15 17:08:28 +0100

    scaffolding

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <>
+            <section>
+                <div className="container mx-auto bg-red-500 h-[1000px]">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate nobis repudiandae obcaecati, esse consectetur sequi velit debitis repellat accusantium harum fugiat nihil quas nemo earum fugit perspiciatis modi distinctio.
+                        Ea totam a officiis maxime debitis quis! Fuga cumque accusamus asperiores laudantium modi qui eum quasi! Neque blanditiis, omnis saepe adipisci tempora perspiciatis eligendi beatae quibusdam, numquam placeat aut incidunt.
+                        Non, quo, tenetur sed minima pariatur dignissimos quia, odit voluptates inventore ipsam quisquam ad eius architecto cumque recusandae veritatis laudantium alias blanditiis doloribus ex expedita fugiat quaerat! Hic, aliquid excepturi.
+                        Ducimus error repudiandae nemo modi, dolorem, neque tempore obcaecati nesciunt magnam reiciendis commodi ea reprehenderit voluptatem! Quis enim, obcaecati laborum quasi itaque minima vitae incidunt quaerat alias voluptatibus, cupiditate commodi!
+                        Explicabo quas minus exercitationem a hic delectus tenetur quae odio assumenda repellendus. Quas eveniet possimus dignissimos, exercitationem ut, nam, officia cupiditate sequi numquam reiciendis atque tempora asperiores rem iusto? Consequuntur!</p>
+                </div>
+            </section>
+        </>
+    )
+}
+export default Home;
