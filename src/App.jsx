a9024409284c54f75e369366c0ba9ed19c64a4dc
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-12-02 18:25:22 +0100

    add header to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import DefaultLayout from "./layouts/DefaultLayout";
+import PokemonIndex from "./pages/Pokemon/PokemonIndex";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<Home />} />
+        <Route index element={<PokemonIndex />} />
       </Route>
     </Routes>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <header className="flex justify-center items-center bg-red-400 h-[80px]">
-            <div className="bg-orange-600 flex justify-center items-center w-[500px] h-[50px] gap-20">
-                <div className="bg-yellow-600 w-[20%] h-[30px] flex justify-center items-center">
-                    <a href="#" className="text-blue-800 font-bold hover:text-sky-600 transition ease-in-out">link 1</a>
-                </div>
-                <div className="bg-yellow-600 w-[20%] h-[30px] flex justify-center items-center">
-                    <a href="#" className="text-blue-800 font-bold hover:text-sky-600 transition ease-in-out">link 2</a>
-                </div>
-                <div className="bg-yellow-600 w-[20%] h-[30px] flex justify-center items-center">
-                    <a href="#" className="text-blue-800 font-bold hover:text-sky-600 transition ease-in-out">link 3</a>
-                </div>
+        <header className="relative flex h-[80px] justify-between items-center text-white py-6 px-8 md:px-32 bg-[#101720] drop-shadow-md">
+            <a href="#">Pokédex Tcg</a>
+            <div className="flex-1 flex justify-center">
+                <ul className="hidden xl:flex items-center text-[16px] font-semibold gap-9">
+                    <li>Pokédex</li>
+                    <li>TCG</li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+
+            <button className="xl:hidden block text-3xl"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <RxHamburgerMenu />
+            </button>
+
+
+            <div
+                className={`absolute lg:hidden left-0 w-full top-[100%] bg-red-600 flex flex-col items-center gap-6 font-semibold text-lg transform transition-all duration-600 ${isMenuOpen ? "opacity-100" : "opacity-0"
+                    }`}
+            >
+                <ul>
+                    <li>ciao</li>
+                    <li>ciao</li>
+                    <li>ciao</li>
+                    <li>ciao</li>
+                </ul>
             </div>
         </header>
-    )
-}
+    );
+};
+
 export default Header;
