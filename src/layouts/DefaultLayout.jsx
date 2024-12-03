3f378c10449d2ac3aaaacb3d9d3f3a220269fdea
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-12-03 16:38:08 +0100

    enhanced useEffect to hamburger menu

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import { useHamburger } from "../contexts/HamburgerContext";
@@ -5,6 +6,25 @@ import { useHamburger } from "../contexts/HamburgerContext";
 const DefaultLayout = () => {
 
     const { isOpen, setIsOpen } = useHamburger(false);
+
+    // Funzione per rimontare ogni volta il main quando l'hamburger menù è sopra i 768px
+    useEffect(() => {
+        const handleResize = () => {
+            // Chiudo il menu se la larghezza della finestra è >= 768px
+            if (window.innerWidth >= 768 && isOpen) {
+                setIsOpen(false);
+            }
+        };
+
+        // Aggiungo l'event listener all'evento resize
+        window.addEventListener("resize", handleResize);
+
+        // Rimuovo l'event listener quando il componente viene smontato
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+
+    }, [isOpen]);
 
     return (
         <>
