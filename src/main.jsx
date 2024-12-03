bb79d1274bbcb43baebb56a13840e0c90b1fbdf6
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-12-03 12:30:10 +0100

    imported hamburger provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HamburgerProvider } from './contexts/HamburgerContext.jsx'
 import './css/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <App />
+      <HamburgerProvider>
+
+        <App />
+
+      </HamburgerProvider>
     </BrowserRouter>
-  </StrictMode >,
+  </StrictMode >
 )
 
 
