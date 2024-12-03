cd1efdd42c809ba79e7618414b7f07ee732ae321
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-12-03 13:16:35 +0100

    fixed hamburger menu opening

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,25 @@
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
 
 const Header = ({ isOpenMenu, setIsOpenMenu }) => {
 
     return (
-        <header className={`${isOpenMenu ? "h-screen" : "h-[80px]"} transform transition-all duration-600 relative flex justify-between items-center text-white py-6 px-8 md:px-32 bg-[#101720] drop-shadow-md`}>
+        <header className={`${isOpenMenu ? "h-screen items-start" : "h-[80px]"} xl:h-[80px] transform transition-all duration-600 flex justify-between text-white py-6 px-8 md:px-32 bg-[#101720] drop-shadow-md`}>
             <a href="#">Pokédex Tcg</a>
-            <div className="flex-1 flex justify-center">
-                <ul className="hidden xl:flex items-center text-[16px] font-semibold gap-9">
+            <div className="flex-1 xl:flex justify-center hidden">
+                <ul className="flex items-center text-[16px] font-semibold gap-9">
                     <li>Pokédex</li>
                     <li>TCG</li>
                     <li>About Us</li>
                 </ul>
             </div>
 
-            <button className="xl:hidden block text-3xl"
+
+            <Link type="button" className="xl:hidden inline text-3xl"
                 onClick={() => setIsOpenMenu(!isOpenMenu)}>
                 <RxHamburgerMenu />
-            </button>
+            </Link>
 
-
-            {/* <div
-                className={`absolute lg:hidden left-0 w-full top-[100%] bg-red-600 flex flex-col items-center gap-6 font-semibold text-lg transform transition-all duration-600 ${isOpenMenu ? "opacity-100" : "invisible opacity-0"
-                    }`}
-            >
-                <ul>
-                    <li>ciao</li>
-                    <li>ciao</li>
-                    <li>ciao</li>
-                    <li>ciao</li>
-                </ul>
-            </div> */}
         </header>
     );
 };
