fe634dadc83c9e49f7157a69bb5520ab64482839
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-12-04 18:52:39 +0100

    added fetch pokemon by id

--- src/pages/Pokedex/PokedexShow.jsx
+++ src/pages/Pokedex/PokedexShow.jsx
@@ -1,3 +1,44 @@
-const PokedexShow = () => { }
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const PokedexShow = () => {
+
+    const { id } = useParams();
+
+    const [pokemon, setPokemon] = useState();
+
+    const fetchPokemon = async () => {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const pokemon = res.data;
+
+        setPokemon(pokemon);
+    }
+
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
+
+    return (
+        <section>
+
+            <div className="container mx-auto">
+
+                <ul>
+                    <li>
+                        <p>Name: <span className="capitalize">{pokemon.name}</span></p>
+                    </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+            </div>
+
+        </section>
+    )
+}
 
 export default PokedexShow;
