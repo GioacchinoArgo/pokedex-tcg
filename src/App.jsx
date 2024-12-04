be2b17a76a88fde9750d29efd409acbb00e51c33
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-12-04 18:51:51 +0100

    added route pokedex show

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import PokedexIndex from "./pages/Pokedex/PokedexIndex";
 import DefaultLayout from "./layouts/DefaultLayout";
+import PokedexShow from "./pages/Pokedex/PokedexShow";
 
 const App = () => {
   return (
     <Routes>
+
+      {/* Pokedex */}
       <Route path="/" element={<DefaultLayout />}>
+
+        {/* Index */}
         <Route index element={<PokedexIndex />} />
+
+        {/* Show */}
+        <Route path=":id" element={<PokedexShow />} />
+
       </Route>
+
     </Routes>
   )
 }
