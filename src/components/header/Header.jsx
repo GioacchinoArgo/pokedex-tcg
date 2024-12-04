c5cdd4746fe58671125b0a9bbb8cb2af9dfc3d5f
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-12-04 16:52:14 +0100

    fixed other issue on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,32 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineClose } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = ({ isOpenMenu, setIsOpenMenu }) => {
     return (
-        <header className={`${isOpenMenu ? "h-screen" : "h-[80px]"} md:h-[80px] transform transition-all duration-700 text-white py-6 md:px-32 bg-[#101720] drop-shadow-md`}>
-            <nav className="container md:h-full mx-auto flex items-center justify-between px-8">
+        <header className={`${isOpenMenu ? "h-screen" : "h-[80px]"} md:h-[80px] transform transition-all duration-700 text-white py-6 md:py-0 md:px-20 bg-[#101720] drop-shadow-md`}>
+            <nav className="container md:h-full mx-auto flex items-center justify-between">
                 {/* Logo */}
                 <Link className="text-2xl font-bold">Pokédex Tcg</Link>
 
                 {/* Navbar Desktop */}
-                <div className="hidden md:h-full md:flex justify-center items-center flex-1">
-                    <ul className="flex md:h-full items-center text-[16px] font-semibold gap-9">
-                        <li><Link className="hover:text-gray-400 transition">Pokédex</Link></li>
-                        <li><Link className="hover:text-gray-400 transition">TCG</Link></li>
-                        <li><Link className="hover:text-gray-400 transition">About Us</Link></li>
+                <div className="hidden md:h-full md:flex justify-end items-center">
+                    <ul className="flex md:h-full items-center text-[17px] font-semibold gap-10">
+                        <li className="md:w-[100px] text-center md:h-full">
+                            <NavLink to={"/"} className="transition md:h-full md:flex md:items-center md:justify-center border-transparent border-b-4 active:border-b-[#E3350D]">
+                                Pokédex
+                            </NavLink>
+                        </li>
+                        <li className="md:w-[100px] text-center md:h-full">
+                            <NavLink className="transition md:h-full md:flex md:items-center md:justify-center border-transparent border-b-4 active:border-b-[#E6BC2F]">
+                                TCG
+                            </NavLink>
+                        </li>
+                        <li className="md:w-[100px] text-center md:h-full">
+                            <NavLink className="transition md:h-full md:flex md:items-center md:justify-center border-transparent border-b-4 active:border-b-[#4DAD5B]">
+                                About Us
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
 
@@ -32,9 +44,9 @@ const Header = ({ isOpenMenu, setIsOpenMenu }) => {
             <div className={`md:hidden flex-col font-semibold text-center text-[#101720] bg-white mt-10 ${isOpenMenu ? "flex" : "hidden"}`}>
                 <Link className="py-4 text-2xl w-full border-l-4 border-[#E3350D] hover:bg-[#E3350D] hover:text-white transition duration-400">Pokédex</Link>
                 <div className="h-[1px] bg-gray-400"></div>
-                <Link className="py-4 text-2xl w-full border-l-4 border-l-[#E6BC2F] border-[#E6BC2F] hover:bg-[#E6BC2F] hover:text-white transition duration-400">TCG</Link>
+                <Link className="py-4 text-2xl w-full border-l-4 border-[#E6BC2F] hover:bg-[#E6BC2F] hover:text-white transition duration-400">TCG</Link>
                 <div className="h-[1px] bg-gray-400"></div>
-                <Link className="py-4 text-2xl w-full border-l-4 border-l-[#4DAD5B] border-[#4DAD5B] hover:bg-[#4DAD5B] hover:text-white transition duration-400">About Us</Link>
+                <Link className="py-4 text-2xl w-full border-l-4 border-[#4DAD5B] hover:bg-[#4DAD5B] hover:text-white transition duration-400">About Us</Link>
             </div>
         </header>
     );
