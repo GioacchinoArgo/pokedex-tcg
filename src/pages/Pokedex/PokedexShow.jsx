6d7f00740bfc79dc58885541d1ed9c84af4f10e0
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-12-09 19:15:07 +0100

    added pokemon evolutions

--- src/pages/Pokedex/PokedexShow.jsx
+++ src/pages/Pokedex/PokedexShow.jsx
@@ -8,9 +8,63 @@ const PokedexShow = () => {
 
     const [pokemon, setPokemon] = useState();
 
-    const fetchPokemon = async () => {
+    // Funzione ricorsiva per ottenere tutta la linea evolutiva
+    const getAllEvolutions = async (chain, evolutions = []) => {
+
+        const evolutionPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${chain.species.name}`);
+
+        // Chiamata per i tipi
+        const types = await Promise.all(
+
+            evolutionPokemon.data.types.map(async ({ type }) => {
+
+                // Prendo tutti i tipi
+                const typeRes = await axios.get(type.url);
+
+                // Cerco l'immagine per ogni tipo, se c'è, se no metto ''
+                const image = typeRes.data.sprites?.['generation-viii']?.['sword-shield']?.name_icon || '';
+
+                // restituisco un oggetto con nome e immagine
+                return { name: type.name, image }
+            })
+        )
+
+        const evolutionInfo = {
+            name: chain.species.name,
+            image: evolutionPokemon.data.sprites.other['official-artwork'].front_default,
+            types
+        }
+
+        // Aggiungi il Pokémon attuale alla lista delle evoluzioni
+        evolutions.push(evolutionInfo);
+
+        // Se ci sono evoluzioni, vai avanti nella catena
+        if (chain.evolves_to.length > 0) {
+            await Promise.all(
+                chain.evolves_to.map(async (subChain) => {
+                    await getAllEvolutions(subChain, evolutions);  // Aggiungi ricorsione
+                })
+            );
+        }
+
+        return evolutions;
+    };
+
+    // Funzione per cercare il Pokemon
+    const fetchPokemon = async (id) => {
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
         let pokemon = res.data;
+
+        // Chiamata per ottenere la specie e l'URL della catena evolutiva
+        const speciesRes = await axios.get(pokemon.species.url);
+        const evolutionUrl = speciesRes.data.evolution_chain.url;
+
+        // Chiamata per ottenere i dati della catena evolutiva
+        const evolutionRes = await axios.get(evolutionUrl);
+        const evolutionData = evolutionRes.data.chain;
+
+        // Ottieni tutte le evoluzioni
+        const evolutions = await getAllEvolutions(evolutionData);
 
         // Chiamata per i tipi
         const dataTypes = await Promise.all(
@@ -42,13 +96,13 @@ const PokedexShow = () => {
         const category = pokemonDescription.genera.filter(category => category.language.name === 'en');
 
         // Creo l'oggeto con tutte le chiavi che mi servono
-        pokemon = { ...pokemon, dataTypes, descriptions, category }
+        pokemon = { ...pokemon, dataTypes, descriptions, category, evolutions, totalEvolution: evolutions.length }
         setPokemon(pokemon);
         console.log(pokemon);
     }
 
     useEffect(() => {
-        fetchPokemon();
+        fetchPokemon(id);
     }, []);
 
     return (
@@ -100,6 +154,46 @@ const PokedexShow = () => {
                     {/* Abilità */}
                     <li>
                         <p className="text-xl">Abilità: <span className="capitalize">{pokemon?.abilities[0].ability.name}</span></p>
+                    </li>
+
+                    {/* Stats */}
+                    <li>
+                        <h4 className="text-2xl">Statistics</h4>
+                        <ul>
+                            {pokemon?.stats.map((stat, i) => (
+                                <li key={`stat-${i}`}>
+                                    <p className="capitalize">{stat.stat.name}: {stat.base_stat}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+
+                    {/* Linea Evolutiva */}
+                    <li className="w-4/5 h-[400px] bg-slate-600 my-16 rounded-md p-6">
+                        <div className="h-full w-full rounded-md flex justify-around gap-10 relative">
+
+                            {pokemon?.evolutions.map((evolution, i) => (
+                                <>
+                                    <div key={`pokemon-${i}`} className={`basis-1/${pokemon.totalEvolution} bg-orange-300 p-4 rounded-full`}>
+                                        <figure className="h-3/4 overflow-hidden flex justify-center">
+                                            <img className="h-full" src={evolution.image} alt={evolution.name} />
+                                        </figure>
+                                        <h2 className="capitalize text-center text-xl">{evolution.name}</h2>
+                                        <div className="flex gap-2 justify-center items-center my-4">
+
+                                            {evolution.types.map(({ image, name }, i) => (
+                                                <div key={`type-${i}`}>
+                                                    <img src={image} alt={name} className="h-6 w-[95px] rounded-md" />
+                                                </div>
+                                            ))}
+
+                                        </div>
+
+                                    </div>
+                                </>
+                            ))}
+
+                        </div>
                     </li>
 
                     {/* Descrizione */}
