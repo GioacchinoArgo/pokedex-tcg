22c630bdf53c621c2f09b70d91bd1983cfa2d492
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-12-11 18:37:15 +0100

    added home route and view

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import PokedexIndex from "./pages/Pokedex/PokedexIndex";
 import DefaultLayout from "./layouts/DefaultLayout";
 import PokedexShow from "./pages/Pokedex/PokedexShow";
 import TcgIndex from "./pages/PokemonTcg/TcgIndex";
+import Home from "./pages/Home";
 
 
 const App = () => {
   return (
     <Routes>
 
-      {/* Pokedex */}
+      {/* Home */}
       <Route path="/" element={<DefaultLayout />}>
+
+        <Route index element={<Home />} />
+
+      </Route>
+
+      {/* Pokedex */}
+      <Route path="/pokedex" element={<DefaultLayout />}>
 
         {/* Index */}
         <Route index element={<PokedexIndex />} />
