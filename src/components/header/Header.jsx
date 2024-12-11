519f3ea62c694b8ba9af52f72ad2f8c828fe19ac
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-12-11 18:56:33 +0100

    added hover effect on links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,29 +4,33 @@ import { Link, NavLink } from "react-router-dom";
 import aboutUs from "../../assets/img/about-us.png";
 import cards from "../../assets/img/cards.png";
 import pokedex from "../../assets/img/pokedex.png";
+import logo from "../../assets/img/logo.png";
 
 const Header = ({ isOpenMenu, setIsOpenMenu }) => {
     return (
         <header className={`${isOpenMenu ? "h-screen" : "h-[80px]"} md:h-[80px] transform transition-all duration-700 text-white py-6 md:py-0 md:px-20 bg-[#101720] drop-shadow-md`}>
             <nav className="container md:h-full mx-auto flex items-center justify-between">
                 {/* Logo */}
-                <Link className="text-2xl font-bold ml-3">Pokédex Tcg</Link>
+                <Link className="text-4xl md:uppercase font-bold ml-3 flex items-center md:pt-[4px]">
+                    {/* <img className="h-12 me-2" src={logo} alt="about-us" /> */}
+                    Pokédex TCG
+                </Link>
 
                 {/* Navbar Desktop */}
                 <div className="hidden md:h-full md:flex justify-end items-center">
-                    <ul className="flex md:h-full items-center text-[17px] font-semibold gap-10">
+                    <ul className="flex md:h-full items-center text-[17px] gap-10">
                         <li className="md:w-[100px] text-center md:h-full">
-                            <NavLink to={"/"} className="transition md:h-full md:flex md:items-center md:justify-center red">
+                            <NavLink to={"/"} className="transition md:h-full md:flex md:items-center border-b-[4px] border-transparent pt-[4px] md:justify-center hover:border-[#E3350D] red">
                                 Pokédex
                             </NavLink>
                         </li>
                         <li className="md:w-[100px] text-center md:h-full">
-                            <NavLink to={"/tcg"} className="transition md:h-full md:flex md:items-center md:justify-center yellow">
+                            <NavLink to={"/tcg"} className="transition md:h-full md:flex md:items-center border-b-[4px] border-transparent pt-[4px] md:justify-center hover:border-[#E6BC2F] yellow">
                                 TCG
                             </NavLink>
                         </li>
                         <li className="md:w-[100px] text-center md:h-full">
-                            <NavLink to={"/about"} className="transition md:h-full md:flex md:items-center md:justify-center green">
+                            <NavLink to={"/about"} className="transition md:h-full md:flex md:items-center border-b-[4px] border-transparent pt-[4px] md:justify-center hover:border-[#4DAD5B] green">
                                 About Us
                             </NavLink>
                         </li>
@@ -44,18 +48,30 @@ const Header = ({ isOpenMenu, setIsOpenMenu }) => {
             </nav>
 
             {/* Navbar Mobile */}
-            <div className={`md:hidden flex-col font-semibold transform transition-all duration-500 flex text-[#101720] bg-white mt-10 ${isOpenMenu ? "" : "invisible opacity-0"}`}>
-                <Link className="py-4 pl-6 flex items-center text-[18px] font-bold w-full border-l-8 border-[#E3350D]">
+            <div className={`md:hidden flex-col font-semibold transform transition-all duration-500 flex text-[#101720] bg-white mt-6 ${isOpenMenu ? "" : "invisible opacity-0"}`}>
+                <Link
+                    to={"/"}
+                    className="py-4 pl-6 flex items-center text-[18px] font-bold w-full border-l-8 border-[#E3350D]"
+                    onClick={() => setIsOpenMenu(!isOpenMenu)}
+                >
                     <img className="h-12 me-4" src={pokedex} alt="about-us" />
                     Pokédex
                 </Link>
                 <div className="h-[1px] bg-gray-400"></div>
-                <Link className="py-4 pl-6 flex items-center text-[18px] font-bold text-2xl w-full border-l-8 border-y-1-[9ca3af] border-[#E6BC2F]">
+                <Link
+                    to={"/tcg"}
+                    className="py-4 pl-6 flex items-center text-[18px] font-bold text-2xl w-full border-l-8 border-y-1-[9ca3af] border-[#E6BC2F]"
+                    onClick={() => setIsOpenMenu(!isOpenMenu)}
+                >
                     <img className="h-12 me-4" src={cards} alt="about-us" />
                     TCG
                 </Link>
                 <div className="h-[1px] bg-gray-400"></div>
-                <Link className="py-4 pl-4 flex items-center text-[18px] font-bold w-full border-l-8 border-[#4DAD5B]">
+                <Link
+                    to={"/about"}
+                    className="py-4 pl-4 flex items-center text-[18px] font-bold w-full border-l-8 border-[#4DAD5B]"
+                    onClick={() => setIsOpenMenu(!isOpenMenu)}
+                >
                     <img className="h-12 me-4" src={aboutUs} alt="cards" />
                     About Us
                 </Link>
