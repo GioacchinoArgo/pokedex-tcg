08828c993cf5c26ac55641193a80258d4c03d645
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-12-13 16:22:11 +0100

    added loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+import { useLoader } from "../contexts/LoaderContext";
+
 const Home = () => {
+
+    const { loader, setLoader } = useLoader();
+
+    useEffect(() => {
+        setLoader(true);
+        setTimeout(() => {
+            setLoader(false);
+        }, 500)
+    }, []);
+
     return (
         <section>
-            <div className="container mx-auto">
-                <h1 className="text-4xl text-center">Home</h1>
-            </div>
+
+            {loader ||
+                <div className="container mx-auto">
+                    <h1 className="text-4xl text-center">Home</h1>
+                </div>
+            }
+
         </section>
     )
 }
