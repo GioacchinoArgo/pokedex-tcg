5e817fb61f7b0394acf63ae14e75b36e64db87e3
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-12-18 17:25:51 +0100

    added loader images

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,25 @@
-import { ImSpinner2 } from "react-icons/im";
+import spinloader from "../../assets/img/spinloader.png"
+import spiralRight from "../../assets/img/spiral-right.png"
+import spiralLeft from "../../assets/img/spiral-left.png"
 
 const Loader = () => {
     return (
         <div className="fixed top-0 bottom-0 left-0 right-0 h-full w-full bg-[#221f1f75] z-20 flex items-center justify-center">
-            <ImSpinner2 className="text-[400px] text-[#bf0603] animate-spin " />
-            <span className="text-[200px] font-bold text-[#bf0603] absolute">P</span>
+
+            <figure className="relative h-[450px]">
+
+                <img src={spinloader} alt="Loader" className="w-full h-full" />
+
+                <figure className="h-[125px] absolute left-24 top-[190px] animate-spin">
+                    <img src={spiralLeft} alt="" className="h-full" />
+                </figure>
+
+                <figure className="h-[125px] absolute left-[270px] top-[190px] loader-spin">
+                    <img src={spiralRight} alt="" className="h-full" />
+                </figure>
+
+            </figure>
+
         </div>
     )
 }
